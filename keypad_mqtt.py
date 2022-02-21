892329c75b2ea3cf954082ab7e5bbad5f4fc2073
Joe Sadusk <joe@sadusk.com>
2022-02-21 08:38:14 +0000

    First working version

---
+++ keypad_mqtt.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+from matrix_keypad import RPi_GPIO
+
+kp = RPi_GPIO.keypad(columnCount = 3)
+import paho.mqtt.client as mqtt
+
+client = mqtt.Client(client_id="keypad")
+client.username_pw_set("keypad", password="Cu1X8X7nwGy7")
+client.connect("192.168.50.80")
+
+code = ""
+nones = 5
+lastkey = None
+
+client.loop_start();
+while True:
+    digit = kp.getKey()
+
+    if digit is not None and digit != lastkey and nones == 0:
+        
+        nones = 5
+        print("Got %d"%digit)
+        code = code + str(digit)
+
+        if len(code) == 4:
+            print("publishing %s"%code)
+            #client.publish("keypad/code", payload=code)
+            code = ""
+    elif digit is None and nones > 0:
+        nones -= 1
+
+
+    lastkey = digit
+            
